06f91bb56bb9f76e3aff4e27553ed0204c82448f
AlexBorous <alexborousas@gmail.com>
2022-01-27 21:22:59 +0200

    feat: add nav bar to project

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import global from '../global.css';
+    import Nav from '../components/Nav.svelte';
+</script>
+<svelt:head>
+    <title>MovieRama</title>
+</svelt:head>
+<Nav />
+
+<slot />
