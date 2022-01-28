be1bce51ee7fb138e93bce3ae66168d93026d29c
AlexBorous <alexborousas@gmail.com>
2022-01-28 15:46:18 +0200

    feat: add onhover animation on movie cards

--- src/components/MovieCard.svelte
+++ src/components/MovieCard.svelte
@@ -1,38 +1,49 @@
 <script>
-  export let movie;
+	export let movie;
+	export let isHovered = false;
+	function hover() {
+		isHovered = !isHovered;
+	}
 </script>
 
-<div class="movie-card">
-  <a sveltekit:prefetch sveltekit:noscroll href={'/movie/' + movie.id }>
-    <img src={'https://image.tmdb.org/t/p/w500' + movie.poster_path} alt="movie.title">
-  </a>
-  <div class="description">
-    <h2>{movie.title}</h2>
-    <p>{movie.release_date}</p>
-  </div>
+<div
+	class="movie-card"
+	on:mouseenter={hover}
+	on:mouseleave={hover}
+	style="--scale: {isHovered ? 1.2 : 1.0}"
+>
+	<a sveltekit:prefetch sveltekit:noscroll href={'/movie/' + movie.id}>
+		<img src={'https://image.tmdb.org/t/p/w500' + movie.poster_path} alt="movie.title" />
+	</a>
+	<div class="description">
+		<h2>{movie.title}</h2>
+		<p>{movie.release_date}</p>
+	</div>
 </div>
 
 <style>
-    img{
-      width: 100%;
-      height: 30vh;
-      object-fit: cover;
-      border-radius: 1rem;
-      margin-bottom: 1rem;
-    }
-    h2{
-      font-size: 0.9rem;
-    }
-    .description{
-      height: 5vh;
-    }
-    p{
-      font-size: 0.7rem;
-    } 
-    .movie-card{
-      display: flex;
-      flex-direction: column;
-      justify-content: space-around;
-      padding: 1rem;
-    }
+	img {
+		width: 100%;
+		height: 30vh;
+		object-fit: cover;
+		border-radius: 1rem;
+		margin-bottom: 1rem;
+	}
+	h2 {
+		font-size: 0.9rem;
+	}
+	.description {
+		height: 5vh;
+	}
+	p {
+		font-size: 0.7rem;
+	}
+	.movie-card {
+		transition: all 350ms ease-out;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-around;
+		padding: 1rem;
+		transform: scale(var(--scale));
+	}
 </style>
